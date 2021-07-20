6422fcedc8f46d3c58b06c70318e7723a26ada2c
mountwebs <mountwebs@gmail.com>
2021-07-20 14:12:21 +0200

    added tag route to get tag by id

--- routes/tagRoute.js
+++ routes/tagRoute.js
@@ -8,6 +8,12 @@ router.get('/', async (req, res) => {
   res.json(tagData).status(200).end();
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const tagData = await tagController.getTag(id);
+  res.json(tagData).status(200).end();
+});
+
 router.post('/', async (req, res) => {
   const postedTag = await tagController.postTag(req.body);
   res.send(JSON.stringify(postedTag)).status(201).end();
