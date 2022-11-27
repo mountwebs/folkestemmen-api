c6e349b2955368b40a10914df3554e14ab13366a
sail-stian <stian@sail.no>
2022-11-27 21:59:06 +0100

    feat: new connection string

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const { app } = require('./app.js');
 
 require('dotenv').config();
 
-const mongoDBString = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.cqebn.gcp.mongodb.net/${process.env.DB_name}?retryWrites=true&w=majority`;
-// const mongoDBString = `mongodb://localhost:27017/folkestemmen`;
+// const mongoDBString = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.cqebn.gcp.mongodb.net/${process.env.DB_name}?retryWrites=true&w=majority`;
+// const mongoDBString = `mongodb://${process.env.DB_user}:${process.env.DB_password}@164.90.167.109:27017/`;
+const mongoDBString = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.em8lth9.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(
   mongoDBString,
-  { useNewUrlParser: true, useUnifiedTopology: true },
+  { useNewUrlParser: true, useUnifiedTopology: true, dbName: process.env.DB_name},
   app.listen(process.env.PORT || 4000, () =>
     console.log(`App is running at port: ${process.env.PORT || 4000}`)
   )
